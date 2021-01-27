b7f3e97b2e1da82ad25623da2d5e96713324bf38
Alejandro <wds@disnot.com>
2021-01-27 12:08:39 +0800

    update info

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -31,7 +31,7 @@ module.exports = {
     // 主题配置
     nav,
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: '/img/EB-logo.png', // 导航栏logo
+    logo: '/img/logo.png', // 导航栏logo
     repo: 'realwds/vuepress-blog-deploy', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
@@ -77,7 +77,7 @@ module.exports = {
     blogger: {
       // 博主信息，显示在首页侧边栏
       avatar:
-        'https://vercel.disnot.com/img/home.png',
+        'https://cdn.jsdelivr.net/gh/realwds/cdn/img/20201216165755.jpg',
       name: 'realwds',
       slogan: 'Delicate facial features are the beginning of the heart, charming temperament is the abyss of emotion.',
     },
